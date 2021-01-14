c5237506954fe5b8ac4d5f3888dd1c2886e8c011
Emanuele Parlascino <emapage95@gmail.com>
2021-01-14 15:52:45 +0000

    home, gallery, about done

--- src/App.js
+++ src/App.js
@@ -1,27 +1,170 @@
-import React from 'react';
-import './App.css';
+import React from "react";
 import FadeIn from 'react-fade-in';
+import images from './images'
+import Grid from "@material-ui/core/Grid"
+import BackgroundSlideshow from 'react-background-slideshow'
 
-function App() {
+import image1 from './Photos/overlay slide 1.jpg'
+import image2 from './Photos/overlay slide 4.jpg'
+import image3 from './Photos/overlay slide 3.jpg'
+import image4 from './Photos/overlay slide 5.jpg'
+
+
+
+import './App.css';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link,
+  useRouteMatch,
+  useParams
+} from "react-router-dom";
+
+
+
+export default function App() {
   return (
-    <div className="App">     
+    <Router>
+        <Switch>
+          <Route exact path="/">
+            <Landing />
+          </Route>
+          <Route path="/Home">
+            <Home />
+          </Route>
+          <Route path="/About">
+            <About />
+          </Route>    
+        </Switch>
+    </Router>
+  );
+}
+
+function Landing() {
+  return (
+    <div className="App"> 
+    <BackgroundSlideshow images={[ image1, image2, image3, image4 ]} animationDelay = {3000}/>
+    <div className= "overlay">
+      <div className = "landing-content">  
         <div className = "slide-right">
-          <h1 className = "name">
-            RP__Photography
-          </h1>
-        </div>
-        <div className = "slide-left">
-          <h2 className = "line">
-            ________________________
-          </h2>
-        </div>
-        <FadeIn delay = {3300} transitionDuration = {2000}>
-          <button className ="enterButton">
-            ENTER
+          <FadeIn delay = {1500} transitionDuration = {2000}>
+            <h1 className = "name">
+              Roberta P. Food Photography
+            </h1>
+          </FadeIn>
+          <FadeIn delay = {2000} transitionDuration = {2000}>
+            <h3>
+              Food and Brand Photography
+            </h3>
+          </FadeIn> 
+        </div>    
+        <FadeIn delay = {2500} transitionDuration = {2000}>      
+          <button className ="btn">
+              <Link to = "/Home" style = {{color: "#3d2a66", fontFamily: "Cambria, Cochin, Georgia, Times, 'Times New Roman', serif", textDecoration:"none"}}>ENTER</Link>
           </button>
-        </FadeIn>
+         </FadeIn>   
+        </div>
+      </div>     
+    </div> 
+  );
+}
+
+
+
+function Home() {
+
+  let { path, url } = useRouteMatch();
+
+  return (
+    <div className = "home-container">
+      <div className = "header">
+        <h1 className = "name-home">
+              Roberta P. Photography
+        </h1>
+        <nav>
+          <li>
+            <Link to={`${url}/gallery`} className = "Home-nav">Gallery</Link>
+          </li>
+          <li>
+            <Link to={`${url}/about`} className = "Home-nav">About</Link>
+          </li>
+          <li>
+            <Link to={`${url}/work`} className = "Home-nav">Work with Me</Link>
+          </li>
+          <li>
+            <a href = "https://bakedandshot.com" target = "_blank" rel="noopener noreferrer" className = "Home-nav">Blog</a>
+          </li>
+        </nav>
+      </div>
+    
+     
+      
+      <Switch>
+        <Route exact path={path}>
+          <Gallery />
+        </Route>
+        <Route path={`${path}/gallery`}>
+          <Gallery />
+        </Route>
+        <Route path={`${path}/about`}>
+          <About />
+        </Route>
+      </Switch>
+    
+    
+      
+      <footer>
+        <div className = "footer-wrapper">
+          <div className = "single-footer">
+            <h2>Links</h2>
+            <p>Instagram</p>
+            <p>Facebook</p>
+          </div>
+        </div>
+      </footer>     
     </div>
   );
 }
 
-export default App;
+function Gallery() {
+  return(
+      <div className = "grid-container">
+        <Grid
+            container
+            direction="row"
+            justify="center"
+            alignItems="center"
+            spacing = {2}
+          >
+          {images.map(({id, src, title, type}) => <Grid item xs = "auto" key = {id}><img src = {src} className = "item" title= {title}  md = "auto" alt = {title} /></Grid>)}
+        </Grid>
+      </div>
+  )
+}
+
+
+
+function About() {
+
+
+  return ( 
+    <div className = "about-container">
+      <img src = {require(`./Photos/aboutPhoto.jpg`)} className = "about-photo" alt = "That's me! =>"/>
+      <p className = "about-body">
+        Hello! My name is Roberta.
+        <br/><br/>
+        I’ve been working in hospitality for over ten years, and my passion for food has been growing ever since I was a little girl running up and down the family hotel, snooping and searching for all the good culinary secrets!
+        Like many others, 2020 brought me to a crossroads where I had to choose a new path. Photography has always been a favourite hobby of mine, and my camera and I have become a great team in recent years, so a journey into food photography was the natural choice for combining my passions! 
+        <br/><br/>
+        My experience and attention to detail from both hospitality and photography perspectives enables me to understand your needs from both sides of the camera, and deliver mouth-watering images for your brand.
+        <br/><br/>
+        So if you are a restauranteur, bar owner, brand manager, food blogger, cookbook or magazine publisher, or just someone who shares my love of stunning photographs of delicious culinary creations, you’ve come to the right place!
+        </p>
+     
+    </div>
+  );
+}
+
+
+
---
+++ src/images.js
@@ -0,0 +1,27 @@
+const images = [
+    {id: 1, src: require('./Photos/1.jpg')},
+    {id: 2, src: require('./Photos/2.jpg')},
+    {id: 3, src: require('./Photos/3.jpg')},
+    {id: 4, src: require('./Photos/4.jpg')},
+    {id: 5, src: require('./Photos/5.jpg')},
+    {id: 6, src: require('./Photos/6.jpg')},
+    {id: 7, src: require('./Photos/7.jpg')},
+    {id: 8, src: require('./Photos/8.jpg')},
+    {id: 9, src: require('./Photos/9.jpg')},
+    {id: 10, src: require('./Photos/10.jpg')},
+    {id: 11, src: require('./Photos/11.jpg')},
+    {id: 12, src: require('./Photos/12.jpg')},
+    {id: 13, src: require('./Photos/13.jpg')},
+    {id: 14, src: require('./Photos/14.jpg')},
+    {id: 15, src: require('./Photos/15.jpg')},
+    {id: 16, src: require('./Photos/16.jpg')},
+    {id: 17, src: require('./Photos/17.jpg')},
+    {id: 18, src: require('./Photos/18.jpg')},
+    {id: 19, src: require('./Photos/19.jpg')},
+    {id: 20, src: require('./Photos/20.jpg')},
+    {id: 21, src: require('./Photos/21.jpg')},
+    {id: 22, src: require('./Photos/22.jpg')},
+    
+]
+
+export default images
